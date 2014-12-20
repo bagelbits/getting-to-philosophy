2b22148be6feddfeba9389213b1da15c63b12792
Chris Ward <fuzzyLombax@gmail.com>
2014-12-19 16:51:09 -0800

    Neat. That gives you wiki body contents.

--- getting_to_philosophy.py
+++ getting_to_philosophy.py
@@ -8,5 +8,8 @@ test_page = "Philosophy"
 
 wiki_page_json = json.load(urllib2.urlopen("http://en.wikipedia.org/w/api.php?format=json&action=query&continue=&titles={0}&prop=revisions&rvprop=content".format(test_page)))
 
+current_rev = wiki_page_json['query']['pages'].keys()[0]
 
-print json.dumps(wiki_page_json, sort_keys=True, indent=4)
+print wiki_page_json['query']['pages'][current_rev]['revisions'][0]['*']
+
+#print json.dumps(wiki_page_json, sort_keys=True, indent=4)
