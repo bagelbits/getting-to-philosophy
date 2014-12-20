a7a5857dd624a996a1103a4d31475b99609b0ec1
Chris Ward <fuzzyLombax@gmail.com>
2014-12-19 17:18:26 -0800

    Well... I guess just user input

--- getting_to_philosophy.py
+++ getting_to_philosophy.py
@@ -5,19 +5,30 @@ import json
 import urllib2
 import re
 
-test_page = "Philosophy"
-test_page = "study"
+def grab_first_wiki_link(page_name):
+  # Wiki api call for getting the latest revision of a page
+  wiki_page_json = json.load(urllib2.urlopen("http://en.wikipedia.org/w/api.php?format=json&action=query&continue=&titles={0}&prop=revisions&rvprop=content".format(page_name)))
 
-# Wiki api call for getting the latest revision of a page
-wiki_page_json = json.load(urllib2.urlopen("http://en.wikipedia.org/w/api.php?format=json&action=query&continue=&titles={0}&prop=revisions&rvprop=content".format(test_page)))
+  # Since it gives the actual revision number
+  current_rev = wiki_page_json['query']['pages'].keys()[0]
 
-# Since it gives the actual revision number
-current_rev = wiki_page_json['query']['pages'].keys()[0]
+  # Links are enclosed with [[  ]]
+  page_contents = wiki_page_json['query']['pages'][current_rev]['revisions'][0]['*']
+  link_match = re.search(r"\[\[([\w\s\(\)]+)\]\]", page_contents)
 
-# Links are enclosed with [[  ]]
-page_contents = wiki_page_json['query']['pages'][current_rev]['revisions'][0]['*']
-link_match = re.search(r"\[\[([\w\s\(\)]+)\]\]", page_contents)
-link_match.group(1)
+  return link_match.group(1)
 
 
-# Remember to sub spaces with underscore for links
+page_to_search = "Philosophy"
+page_to_search = "Property_(philosophy)".capitalize()
+hop_count = 0
+
+print "http://en.wikipedia.org/wiki/{0}".format(page_to_search)
+
+while page_to_search.lower() != "philosophy":
+  page_to_search = grab_first_wiki_link(page_to_search)
+  page_to_search = re.sub(r'\s', '_', page_to_search.strip()).capitalize()
+  print "http://en.wikipedia.org/wiki/{0}".format(page_to_search)
+  hop_count +=1
+
+print "{} hops".format(hop_count)
